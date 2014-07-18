c52e34703fde6936d43cec40c366ecc48c566fc8
Josh <JoshPullen27@gmail.com>
2014-07-18 12:14:16 -0400

    bug fix

--- js/hypercat.js
+++ js/hypercat.js
@@ -56,7 +56,7 @@ hc.prototype = {
  	move: function(dist) {
  		hc.getSprite(this.sprite).x += Math.sin(hc.getSprite(this.sprite).angle) * dist;
  		hc.getSprite(this.sprite).y += Math.cos(hc.getSprite(this.sprite).angle) * dist;
- 	}
+ 	},
 
  	position: function(x, y) {
 		hc.getSprite(this.sprite).x = x;
